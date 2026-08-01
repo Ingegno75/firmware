41aa42f670956d39aecc7fac2a38166bab08b809
Ingegno75 <ingegno75@gmail.com>
2026-08-01 23:13:39 +0200

    Fix ambiguità namespace WeatherDayForecast

--- src/graphics/niche/InkHUD/Applets/User/Weather/WeatherApplet.cpp
+++ src/graphics/niche/InkHUD/Applets/User/Weather/WeatherApplet.cpp
@@ -253,7 +253,7 @@ void InkHUD::WeatherFetcher::fetchWeather()
 
     for (uint8_t i = 0; i < 3; i++) {
         uint8_t srcIndex = i + 1; // giorno successivo
-        InkHUD::WeatherDayForecast day;
+        WeatherDayForecast day;
 
         if (srcIndex < dates.size()) {
             std::string date = dates[srcIndex].as<std::string>(); // "YYYY-MM-DD"
